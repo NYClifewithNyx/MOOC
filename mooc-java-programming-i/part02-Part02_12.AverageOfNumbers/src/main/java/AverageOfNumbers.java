431de0be02a9f4ad6ef74b9846d2dca03e7bc88a
NYClifewithNyx <nyclifewithnyx@gmail.com>
2026-06-14 17:47:14 -0400

    6/14/2026_Problems Solved

--- mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
+++ mooc-java-programming-i/part02-Part02_12.AverageOfNumbers/src/main/java/AverageOfNumbers.java
@@ -5,10 +5,26 @@ public class AverageOfNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int sum = 0;
+        float average = 0;
+        int count = 0;
         
-        
-
-
+        while (true) {
+            System.out.println("Give a number:");
+            int number = Integer.valueOf(scanner.nextLine());
+            
+            if (number == 0) {
+                System.out.println("Average of the numbers: " + average);
+                
+                break;
+            }
+            
+            sum += number;
+            count++;
+            average = (float) sum / count;
+            
+        }
+       
 
     }
 }
--- mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
+++ mooc-java-programming-i/part02-Part02_13.AverageOfPositiveNumbers/src/main/java/AverageOfPositiveNumbers.java
@@ -5,6 +5,34 @@ public class AverageOfPositiveNumbers {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-
+        int sum = 0;
+        int count = 0;
+        float average = 0; 
+        
+        while (true) {
+            int number = Integer.valueOf(scanner.nextLine());
+            
+           
+            if (number == 0) {
+                
+                if (sum <= 0) {
+                    System.out.println("Cannot calculate the average");
+                    break;
+                }
+                
+                System.out.print(average);
+                break;                                
+            }          
+            
+            if (number >= 0) {
+                sum += number;
+                count++;
+                average = (float) sum / count;    
+            }
+            
+                        
+        }
+             
     }
+        
 }
